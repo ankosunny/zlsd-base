0d5593e45009a8d2bf6b5c7774be3a9bacc7f823
today <12345678>
2019-05-06 19:07:46 +0800

    1、修改eurake注册中心问题 未导入jar包 导致不能注册 2、人工催收 增加下拉接口处理 3、增加查看预分案结果信息接口

--- zlsd-base-common/src/main/java/com/zhilingsd/base/common/constants/BaseDictConstants.java
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/constants/BaseDictConstants.java
@@ -12,10 +12,8 @@ public class BaseDictConstants {
     public static final String BILL_CLASSIFY_STATUS = "bill_classify_status";
     // 定义电话状态
     public static final String TEL_STATUS = "tel_status";
-
     // 定义停催状态
     public static final String BILL_STOP_STATUS = "bill_stop_status";
-
     // 定义案件分配状态
     public static final String ALLOT_STATUS = "allot_status";
     //申请类型
@@ -24,8 +22,6 @@ public class BaseDictConstants {
     public static final String APPROVER_STATUS = "approver_status";
     // 定义手别
     public static final String BACTH_TIME = "bacth_time";
-    //产品类型 批次设置类型
-    public static final String PRODUCT_TYPE = "product_type";
     // 定义外访任务分配状态
     public static final String VISIT_ALLOT_STATUS = "visit_allot_status";
     //外访状态
@@ -39,6 +35,10 @@ public class BaseDictConstants {
     //手机停催状态
     public static final String PHONE_STOP_STATUS = "phone_stop_status";
 
+    //产品类型 批次设置类型
+    public static final String PRODUCT_TYPE = "product_type";
+    //催收流程
+    public static final String COLLECTION_TYPE = "collection_type";
     //产品名称 - 案件导入
     public static final String PRODUCT_NAME = "product_name";
     // 定义账龄种类
--- zlsd-base-common/src/main/java/com/zhilingsd/base/common/emuns/InitParamEnum.java
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/emuns/InitParamEnum.java
@@ -1,6 +1,9 @@
 package com.zhilingsd.base.common.emuns;
 
+import com.google.common.collect.Lists;
 import com.zhilingsd.base.common.constants.BaseDictConstants;
+
+import java.util.List;
 
 /**
  * ^---^---^---^---^---^---^---^
@@ -8,22 +11,102 @@ import com.zhilingsd.base.common.constants.BaseDictConstants;
  *
  * @author zou.cp
  * @version 1.0
- * @Description  下拉参数枚举
+ * @Description 下拉参数枚举
  * @createTime 2019年05月04日 14:22*
  * log.info()
  */
 public enum InitParamEnum {
 
-    BILL_STATUS(1, BaseDictConstants.BILL_STATUS),
-    BILL_CLASSIFY_STATUS(2, BaseDictConstants.BILL_CLASSIFY_STATUS),
-    ALLOT_STATUS(3, BaseDictConstants.ALLOT_STATUS),
-    INIT_1(0, "文员已发送"),
-    INIT_2(0, "文员已发送"),
-    INIT_3(0, "文员已发送"),
-    INIT_4(0, "文员已发送"),
+    /**
+     * @description 批次分配状态
+     **/
+    PACKET_ALLOT_STATUS(1, BaseDictConstants.PACKET_ALLOT_STATUS),
+    /**
+     * @description 案件状态
+     **/
+    BILL_STATUS(2, BaseDictConstants.BILL_STATUS),
+    /**
+     * @description 案件归类状态
+     **/
+    BILL_CLASSIFY_STATUS(3, BaseDictConstants.BILL_CLASSIFY_STATUS),
+    /**
+     * @description 手别
+     **/
+    BACTH_TIME(4, BaseDictConstants.BACTH_TIME),
+    /**
+     * @description 电话状态
+     **/
+    TEL_STATUS(5, BaseDictConstants.TEL_STATUS),
+    /**
+     * @description 案件跟进情况
+     **/
+    BILL_FOLLOW_TYPE(6, BaseDictConstants.BILL_FOLLOW_TYPE),
+    /**
+     * @description 性别
+     **/
+    SEX(7, BaseDictConstants.SEX),
+    /**
+     * @description 外访分配状态
+     **/
+    VISIT_ALLOT_STATUS(8, BaseDictConstants.VISIT_ALLOT_STATUS),
+    /**
+     * @description 外访状态
+     **/
+    VISIT_STATUS(9, BaseDictConstants.VISIT_STATUS),
+    /**
+     * @description 还款状态
+     **/
+    REPAY_STATUS(10, BaseDictConstants.REPAY_STATUS),
+    /**
+     * @description 案件停催状态
+     **/
+    BILL_STOP_STATUS(11, BaseDictConstants.BILL_STOP_STATUS),
+    /**
+     * @description 手机停催状态
+     **/
+    PHONE_STOP_STATUS(12, BaseDictConstants.PHONE_STOP_STATUS),
+    /**
+     * @description 申请类型
+     **/
+    APPLY_TYPE(13, BaseDictConstants.APPLY_TYPE),
+    /**
+     * @description 审批状态
+     **/
+    APPROVER_STATUS(14, BaseDictConstants.APPROVER_STATUS),
+    /**
+     * @description 催收流程
+     **/
+    COLLECTION_TYPE(15,BaseDictConstants.COLLECTION_TYPE),
+    /**
+     * @description  案件分配状态
+     **/
+    ALLOT_STATUS(16,BaseDictConstants.ALLOT_STATUS),
+    /**
+     * @description 地址类型  
+     **/
+    /**
+     * @description  客户关系
+     **/
     ;
 
-    private int code ;
+    /**
+     * @description  案件中心下拉列表
+     **/
+    public static List<String> getBillCenterDictTypes(){
+        List<String> list = Lists.newArrayList(BILL_STATUS.getValue(),BILL_CLASSIFY_STATUS.getValue(),COLLECTION_TYPE.getValue(),
+                BACTH_TIME.getValue(),TEL_STATUS.getValue(),BILL_FOLLOW_TYPE.getValue());
+        return list;
+    }
+    /**
+     * @description  人工催收下拉列表
+     **/
+    public static List<String> getManualDictTypes(){
+        List<String> list = Lists.newArrayList(BILL_STATUS.getValue(),BILL_CLASSIFY_STATUS.getValue(),COLLECTION_TYPE.getValue(),
+                BACTH_TIME.getValue(),TEL_STATUS.getValue(),BILL_FOLLOW_TYPE.getValue());
+        return list;
+    }
+
+    private int code;
     private String value;
 
     InitParamEnum(int code, String value) {
@@ -47,13 +130,14 @@ public enum InitParamEnum {
         this.value = value;
     }
 
-    public static  String getValueByCode(int code){
-        for (InitParamEnum osEnum: InitParamEnum.values()) {
-            if(osEnum.getCode()==code){
-                return  osEnum.getValue();
+    public static String getValueByCode(int code) {
+        for (InitParamEnum osEnum : InitParamEnum.values()) {
+            if (osEnum.getCode() == code) {
+                return osEnum.getValue();
             }
         }
-        return  "";
+        return "";
     }
+
 
 }

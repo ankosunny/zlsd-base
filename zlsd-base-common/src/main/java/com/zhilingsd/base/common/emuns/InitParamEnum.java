c97e461174dd348e5f88ddc5b8e78cc53b7b95c8
today <12345678>
2019-05-07 16:11:53 +0800

    1、下拉功能整理优化 2、增加下拉数据所属判断

--- zlsd-base-common/src/main/java/com/zhilingsd/base/common/emuns/InitParamEnum.java
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/emuns/InitParamEnum.java
@@ -1,6 +1,7 @@
 package com.zhilingsd.base.common.emuns;
 
 import com.google.common.collect.Lists;
+import com.zhilingsd.base.common.bean.KeyValueBean;
 import com.zhilingsd.base.common.constants.BaseDictConstants;
 
 import java.util.List;
@@ -24,125 +25,111 @@ public enum InitParamEnum {
     /**
      * @description 案件状态
      **/
-    BILL_STATUS(2, BaseDictConstants.BILL_STATUS),
+    BILL_STATUS(1, BaseDictConstants.BILL_STATUS),
     /**
      * @description 案件归类状态
      **/
-    BILL_CLASSIFY_STATUS(3, BaseDictConstants.BILL_CLASSIFY_STATUS),
+    BILL_CLASSIFY_STATUS(1, BaseDictConstants.BILL_CLASSIFY_STATUS),
     /**
      * @description 手别
      **/
-    BACTH_TIME(4, BaseDictConstants.BACTH_TIME),
+    BACTH_TIME(1, BaseDictConstants.BACTH_TIME),
     /**
      * @description 电话状态
      **/
-    TEL_STATUS(5, BaseDictConstants.TEL_STATUS),
+    TEL_STATUS(1, BaseDictConstants.TEL_STATUS),
     /**
      * @description 案件跟进情况
      **/
-    BILL_FOLLOW_TYPE(6, BaseDictConstants.BILL_FOLLOW_TYPE),
+    BILL_FOLLOW_TYPE(1, BaseDictConstants.BILL_FOLLOW_TYPE),
     /**
      * @description 性别
      **/
-    SEX(7, BaseDictConstants.SEX),
+    SEX(1, BaseDictConstants.SEX),
     /**
      * @description 外访分配状态
      **/
-    VISIT_ALLOT_STATUS(8, BaseDictConstants.VISIT_ALLOT_STATUS),
+    VISIT_ALLOT_STATUS(1, BaseDictConstants.VISIT_ALLOT_STATUS),
     /**
      * @description 外访状态
      **/
-    VISIT_STATUS(9, BaseDictConstants.VISIT_STATUS),
+    VISIT_STATUS(1, BaseDictConstants.VISIT_STATUS),
     /**
      * @description 还款状态
      **/
-    REPAY_STATUS(10, BaseDictConstants.REPAY_STATUS),
+    REPAY_STATUS(1, BaseDictConstants.REPAY_STATUS),
     /**
      * @description 案件停催状态
      **/
-    BILL_STOP_STATUS(11, BaseDictConstants.BILL_STOP_STATUS),
+    BILL_STOP_STATUS(1, BaseDictConstants.BILL_STOP_STATUS),
     /**
      * @description 手机停催状态
      **/
-    PHONE_STOP_STATUS(12, BaseDictConstants.PHONE_STOP_STATUS),
+    PHONE_STOP_STATUS(1, BaseDictConstants.PHONE_STOP_STATUS),
     /**
      * @description 申请类型
      **/
-    APPLY_TYPE(13, BaseDictConstants.APPLY_TYPE),
+    APPLY_TYPE(1, BaseDictConstants.APPLY_TYPE),
     /**
      * @description 审批状态
      **/
-    APPROVER_STATUS(14, BaseDictConstants.APPROVER_STATUS),
+    APPROVER_STATUS(1, BaseDictConstants.APPROVER_STATUS),
     /**
-     * @description  案件分配状态
+     * @description 案件分配状态
      **/
-    ALLOT_STATUS(15,BaseDictConstants.ALLOT_STATUS),
+    ALLOT_STATUS(1, BaseDictConstants.ALLOT_STATUS),
     /**
-     * @description 地址类型  
+     * @description 地址类型
      **/
-    ADDRESS_TYPE(16,BaseDictConstants.ADDRESS_TYPE),
+    ADDRESS_TYPE(1, BaseDictConstants.ADDRESS_TYPE),
     /**
-     * @description  客户关系
+     * @description 客户关系
      **/
-    CLIENT_RELATION(17,BaseDictConstants.CLIENT_RELATION),
+    CLIENT_RELATION(1, BaseDictConstants.CLIENT_RELATION),
     /**
-     * @description  币种
+     * @description 币种
      **/
-    CURRENCY(18,BaseDictConstants.CURRENCY),
+    CURRENCY(1, BaseDictConstants.CURRENCY),
+/******************** 使用CollectionType作为下拉 ，查询需要通过现有流程进行处理 ****************************/
     /**
      * @description 催收流程
      **/
-    COLLECTION_TYPE(19,BaseDictConstants.COLLECTION_TYPE),
+    COLLECTION_TYPE(1, BaseDictConstants.COLLECTION_TYPE),
 
 /************************ 常量信息表 ***********************************************/
     /**
-     * @description  户籍地址
+     * @description 户籍地址
      **/
-    REGISTERED_ADDRESS(20,BaseDictConstants.REGISTERED_ADDRESS),
+    REGISTERED_ADDRESS(0, BaseDictConstants.REGISTERED_ADDRESS),
     /**
-     * @description  案件地区
+     * @description 案件地区
      **/
-    CASE_AREA(21,BaseDictConstants.CASE_AREA),
+    CASE_AREA(0, BaseDictConstants.CASE_AREA),
     /**
-     * @description  账龄种类
+     * @description 账龄种类
      **/
-    BUCKET(22,BaseDictConstants.BUCKET),
+    BUCKET(0, BaseDictConstants.BUCKET),
     /**
-     * @description  产品名称
+     * @description 产品名称
      **/
-    PRODUCT_NAME(23,BaseDictConstants.PRODUCT_NAME),
+    PRODUCT_NAME(0, BaseDictConstants.PRODUCT_NAME),
     ;
 
     /**
-     * @description  案件中心下拉列表
+     * @description 案件中心下拉列表
      **/
-    public static List<String> getBillCenterDictTypes(){
-        List<String> list = Lists.newArrayList(BILL_STATUS.getValue(),BILL_CLASSIFY_STATUS.getValue(),COLLECTION_TYPE.getValue(),
-                BACTH_TIME.getValue(),TEL_STATUS.getValue(),BILL_FOLLOW_TYPE.getValue());
+    public static List<String> getBillCenterTypes() {
+        List<String> list = Lists.newArrayList(BILL_STATUS.getValue(), BILL_CLASSIFY_STATUS.getValue(), COLLECTION_TYPE.getValue(),
+                BACTH_TIME.getValue(), TEL_STATUS.getValue(), BILL_FOLLOW_TYPE.getValue());
         return list;
     }
+
     /**
-     * @description  案件中心下拉列表-常量
+     * @description 人工催收下拉列表
      **/
-    public static List<String> getBillCenterConstantTypes(){
-        List<String> list = Lists.newArrayList(BILL_STATUS.getValue(),BILL_CLASSIFY_STATUS.getValue(),COLLECTION_TYPE.getValue(),
-                BACTH_TIME.getValue(),TEL_STATUS.getValue(),BILL_FOLLOW_TYPE.getValue());
-        return list;
-    }
-    /**
-     * @description  人工催收下拉列表
-     **/
-    public static List<String> getManualDictTypes(){
-        List<String> list = Lists.newArrayList(BILL_STATUS.getValue(),BILL_CLASSIFY_STATUS.getValue(),COLLECTION_TYPE.getValue(),
-                BACTH_TIME.getValue(),TEL_STATUS.getValue(),BILL_FOLLOW_TYPE.getValue());
-        return list;
-    }
-    /**
-     * @description  人工催收下拉列表-常量
-     **/
-    public static List<String> getManualConstantTypes(){
-        List<String> list = Lists.newArrayList(BILL_STATUS.getValue(),BILL_CLASSIFY_STATUS.getValue(),COLLECTION_TYPE.getValue(),
-                BACTH_TIME.getValue(),TEL_STATUS.getValue(),BILL_FOLLOW_TYPE.getValue());
+    public static List<String> getManualTypes() {
+        List<String> list = Lists.newArrayList(BILL_STATUS.getValue(), BILL_CLASSIFY_STATUS.getValue(), COLLECTION_TYPE.getValue(),
+                BACTH_TIME.getValue(), TEL_STATUS.getValue(), BILL_FOLLOW_TYPE.getValue());
         return list;
     }
 
@@ -177,6 +164,30 @@ public enum InitParamEnum {
             }
         }
         return "";
+    }
+
+    /**
+     * @description 判断是否是字典数据
+     **/
+    public static boolean isDict(String value) {
+        for (InitParamEnum osEnum : InitParamEnum.values()) {
+            if (value.equals(osEnum.getValue()) && osEnum.getCode() == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @description 判断是否是常量数据
+     **/
+    public static boolean isConstant(String value) {
+        for (InitParamEnum osEnum : InitParamEnum.values()) {
+            if (value.equals(osEnum.getValue()) && osEnum.getCode() == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 
 

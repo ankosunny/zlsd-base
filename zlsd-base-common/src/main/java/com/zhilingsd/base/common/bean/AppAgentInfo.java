1b5f7900559abf4e5018bc47d1389220d88c6400
jacky <1073740572@qq.com>
2019-05-18 19:23:57 +0800

    优化swagger模块

--- zlsd-base-common/src/main/java/com/zhilingsd/base/common/bean/AppAgentInfo.java
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/bean/AppAgentInfo.java
@@ -23,6 +23,9 @@ public class AppAgentInfo {
 
     private long resourceId;
 
+    public AppAgentInfo() {
+    }
+
     public AppAgentInfo(long operatorId, long collectionCompanyId) {
         this.operatorId = operatorId;
         this.collectionCompanyId = collectionCompanyId;

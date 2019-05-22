dd75572fba1c68e7f644a37d594fec1ca1d7e715
today <12345678>
2019-05-22 11:56:42 +0800

    1、redis配置处理 2、增加批量修改催收流程(不修改案件状态，判断人工催收流程标识处理) 3、案件信息更新 ， 案件催收流程更新 4、申请业务去申请人信息

--- zlsd-base-common/src/main/java/com/zhilingsd/base/common/utils/tree/ConditionTreeBean.java
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/utils/tree/ConditionTreeBean.java
@@ -7,15 +7,6 @@ public class ConditionTreeBean extends TreeNode implements Serializable{
     private static final long serialVersionUID = 3887559806021822087L;
     private Long id;
     private String name;
-    private Integer functionType;//(0-普通机构，1-预测式外呼机构)
-
-    public Integer getFunctionType() {
-        return functionType;
-    }
-
-    public void setFunctionType(Integer functionType) {
-        this.functionType = functionType;
-    }
 
     public Long getId() {
         return id;

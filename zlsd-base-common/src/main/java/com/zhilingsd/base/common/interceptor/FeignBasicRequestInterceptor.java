62bf73e954784155db2fa46a22da0b4c71a4acc6
today <12345678>
2019-05-23 16:25:59 +0800

    1、SWAGGER文档接口设置请求头信息 2、标记函件完成处理 ， 作业管理申请处理 3、请求头信息拦截 4、增加拦截器日志信息

--- zlsd-base-common/src/main/java/com/zhilingsd/base/common/interceptor/FeignBasicRequestInterceptor.java
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/interceptor/FeignBasicRequestInterceptor.java
@@ -3,6 +3,7 @@ package com.zhilingsd.base.common.interceptor;
 import com.zhilingsd.base.common.utils.AppUtil;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Objects;
 
@@ -11,6 +12,7 @@ import java.util.Objects;
  * @author linmenghuai
  * @date 2019-5-14 20:00:40
  * */
+@Slf4j
 public class FeignBasicRequestInterceptor implements RequestInterceptor {
 
     /**
@@ -25,6 +27,12 @@ public class FeignBasicRequestInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
+        if (Objects.isNull(AppUtil.getAppAgentInfo())) {
+            log.info("拦截器没有当前登录用户信息");
+        }else {
+            log.info(OPERATOR_ID +": "+AppUtil.getAppAgentInfo().getOperatorId());
+            log.info(COLLECTION_COMPANY_ID +": "+AppUtil.getAppAgentInfo().getCollectionCompanyId());
+        }
         if (Objects.nonNull(AppUtil.getAppAgentInfo().getOperatorId()) && Objects.nonNull(AppUtil.getAppAgentInfo().getCollectionCompanyId())){
             requestTemplate.header(OPERATOR_ID, AppUtil.getAppAgentInfo().getOperatorId()+"");
             requestTemplate.header(COLLECTION_COMPANY_ID, AppUtil.getAppAgentInfo().getCollectionCompanyId()+"");

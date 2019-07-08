da2cd7ea20338d7ce145a39f719f51fd86b5344d
tangj <939903962@qq.com>
2019-07-08 18:15:20 +0800

    增加判断对象所有属性是否为空

--- zlsd-base-common/src/main/java/com/zhilingsd/base/common/utils/jsonserializer/ObjectCheckUtils.java
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/utils/jsonserializer/ObjectCheckUtils.java
@@ -137,4 +137,29 @@ public class ObjectCheckUtils {
         }
         return false;
     }
+
+    /**
+     * @description: 判断该对象是否: 返回ture表示所有属性为null  返回false表示不是所有属性都是null
+     * @author: tangj
+     * @create: 2019/07/08 16:30
+     */
+    public static boolean isAllFieldNull(Object obj){
+        boolean flag = true;
+        try {
+            Class stuCla = (Class) obj.getClass();// 得到类对象
+            Field[] fs = stuCla.getDeclaredFields();//得到属性集合
+
+            for (Field f : fs) {//遍历属性
+                f.setAccessible(true); // 设置属性是可以访问的(私有的也可以)
+                Object val = f.get(obj);// 得到此属性的值
+                if(val!=null) {//只要有1个属性不为空,那么就不是所有的属性值都为空
+                    flag = false;
+                    break;
+                }
+            }
+        }catch (Exception e){
+            log.error("判断对象的所有属性是否为空出现异常！", e);
+        }
+        return flag;
+    }
 }

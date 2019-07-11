815f3d602a0ef049cdcafd77cdf92ce49dd79f54
zouchangpeng <12345678>
2019-07-11 09:03:44 +0800

    1、导入导出管理增加枚举类型

--- zlsd-base-common/src/main/java/com/zhilingsd/base/common/emuns/billmanage/ImpExpManageSubtypeEnum.java
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/emuns/billmanage/ImpExpManageSubtypeEnum.java
@@ -18,6 +18,14 @@ public enum ImpExpManageSubtypeEnum {
     EXP_SINGLE_MARK("exp_single_mark","导出单户催记"),
     EXP_RECORDINGS("exp_recordings","导出录音"),
     EXP_BILL_CENTER("exp_bill_center","导出案件（案件中心）"),
+    /////////////还款相关///////////////////
+    IMP_REPAY_FLOW("imp_repay_record","导入还款流水（还款）"),
+    IMP_REPAY_FLOW_BILL("imp_repay_record_bill","导入还款流水账单（还款）"),
+    IMP_REPAY_BILL("imp_repay_bill","导入还款账单（还款）"),
+    EXP_REPAY_FLOW("exp_repay_record","导出还款流水（还款）"),
+    EXP_REPAY_FLOW_BILL("exp_repay_record_bill","导出还款流水账单（还款）"),
+    EXP_REPAY_BILL("exp_repay_bill","导出还款账单（还款）"),
+    DEL_REPAY_FLOW("del_repay_bill","删除还款流水（还款）"),
     ;
 
     private String code;

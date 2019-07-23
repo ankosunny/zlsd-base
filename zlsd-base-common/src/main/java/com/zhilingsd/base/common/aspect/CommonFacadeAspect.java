8a7a8fe44da2f5396ce626cd8a99d0acce37286a
zhangrong <gcmr@qq.com>
2019-07-23 16:08:32 +0800

    控制接口入参打印

--- zlsd-base-common/src/main/java/com/zhilingsd/base/common/aspect/CommonFacadeAspect.java
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/aspect/CommonFacadeAspect.java
@@ -8,6 +8,7 @@ import com.zhilingsd.base.common.utils.AppUtil;
 import com.zhilingsd.base.common.utils.IPUtils;
 import com.zhilingsd.base.common.utils.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -17,6 +18,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.Optional;
 
 /**
@@ -35,6 +37,8 @@ public class CommonFacadeAspect {
 //    protected Validator validator;
     private static final String PRE_TAG = "************** ";
 
+    private static final String UNPRINT_CLASS = "com.zhilingsd.blackhole.dto.request.UploadWithBase64Request";
+
     @Around(value = "@annotation(com.zhilingsd.base.common.annotation.CommonFacade)")
     public Object before(ProceedingJoinPoint jp) throws Throwable {
         long startTime = System.currentTimeMillis();
@@ -46,7 +50,13 @@ public class CommonFacadeAspect {
         sb.append("\n" + PRE_TAG + "HTTP_METHOD : " + request.getMethod());
         sb.append("\n" + PRE_TAG + "IP地址 : " + IPUtils.getRemortIP(request));
         sb.append("\n" + PRE_TAG + jp.getSignature().getDeclaringTypeName() + "." + jp.getSignature().getName());
-        sb.append("\n" + PRE_TAG + "接口入参 : " + JsonUtils.toJsonString(jp.getArgs()));
+
+        if (isPrintArgs(jp.getArgs())) {
+            sb.append("\n" + PRE_TAG + "接口入参 : " + JsonUtils.toJsonString(jp.getArgs()));
+        } else {
+            sb.append("\n" + PRE_TAG + "接口入参 : 不打印入参");
+        }
+
         log.info(sb.toString());
         // 创建AppAgentInfo对象，如果有字段为空则抛出异常
         String session = Optional.ofNullable(request.getHeader("session")).orElseThrow(() -> new ServiceException(ReturnCode.BUSINESS_ERROR.getCode(), "请求头session不能为空"));
@@ -62,12 +72,20 @@ public class CommonFacadeAspect {
 
         StringBuffer sbreturn = new StringBuffer();
         sbreturn.append("\n" + PRE_TAG + jp.getSignature().getDeclaringTypeName() + "." + jp.getSignature().getName());
-        sbreturn.append("\n" + PRE_TAG +  " 接口返回 : " + JsonUtils.toJsonString(obj));
+        sbreturn.append("\n" + PRE_TAG + " 接口返回 : " + JsonUtils.toJsonString(obj));
         sbreturn.append("\n" + PRE_TAG + " 花费时间 : " + (System.currentTimeMillis() - startTime) + "ms");
         log.info(sbreturn.toString());
         return obj;
     }
 
+    private Boolean isPrintArgs(Object[] objects) {
+        try {
+            return Arrays.stream(objects).noneMatch(object -> StringUtils.equals(object.getClass().getName(), UNPRINT_CLASS));
+        } catch (Exception ex) {
+            log.warn("判断是否输出接口入参出错，ex:{}", ex.toString());
+            return true;
+        }
+    }
 
 //    /**
 //     * 参数校验

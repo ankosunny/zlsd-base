953fb403c147df450126d3c85ac66e515ac23273
yangguojun <1073740572@qq.com>
2019-07-26 22:42:55 +0800

    解决导入联系人bug

--- zlsd-base-common/src/main/java/com/zhilingsd/base/common/support/GlobalExceptionHandler.java
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/support/GlobalExceptionHandler.java
@@ -264,7 +264,6 @@ public class GlobalExceptionHandler {
     }
 
 
-
     /**
      * 捕获处理IllegalParameterException
      *
@@ -303,12 +302,12 @@ public class GlobalExceptionHandler {
     /**
      * mybatis异常
      */
-    @ExceptionHandler(value = MyBatisSystemException.class)
+/*    @ExceptionHandler(value = MyBatisSystemException.class)
     public CollectionResult myBatisSystemException(MyBatisSystemException ex) {
         ex.printStackTrace();
         log.error("mybatis异常,异常信息：", ex);
         return CollectionResult.failed(ReturnCode.ERROR_500.getCode(), ex.toString());
-    }
+    }*/
 
 
     /**

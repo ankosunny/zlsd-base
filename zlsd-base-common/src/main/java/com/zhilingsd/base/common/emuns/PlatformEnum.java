4f9aa10b859c1024a7190ec31cfa09c49e2519f2
yuboliang <12345678>
2019-09-03 15:06:18 +0800

    操作平台枚举

---
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/emuns/PlatformEnum.java
@@ -0,0 +1,59 @@
+/**
+ * Software License Declaration.
+ * <p>
+ * zhilingsd.com, Co,. Ltd.
+ * Copyright © 2019 All Rights Reserved.
+ * <p>
+ * Copyright Notice
+ * This documents is provided to zhilingsd contracting agent or authorized programmer only.
+ * This source code is written and edited by zhilingsd Co,.Ltd Inc specially for financial
+ * business contracting agent or authorized cooperative company, in order to help them to
+ * install, programme or central control in certain project by themselves independently.
+ * <p>
+ * Disclaimer
+ * If this source code is needed by the one neither contracting agent nor authorized programmer
+ * during the use of the code, should contact to zhilingsd Co,. Ltd Inc, and get the confirmation
+ * and agreement of three departments managers  - Research Department, Marketing Department and
+ * Production Department.Otherwise zhilingsd will charge the fee according to the programme itself.
+ * <p>
+ * Any one,including contracting agent and authorized programmer,cannot share this code to
+ * the third party without the agreement of wandaph. If Any problem cannot be solved in the
+ * procedure of programming should be feedback to zhilingsd Co,. Ltd Inc in time, Thank you!
+ */
+package com.zhilingsd.base.common.emuns;
+
+/**
+ * @author ZhangRong
+ * @version Id: com.collection.zhilingsd.api.common.enums.PlatformEnum, v 0.1 2019/5/22 23:22 ZhangRong Exp $$
+ */
+public enum PlatformEnum {
+
+    COLLECTION("collection", "催收系统"),
+    CONSOLE("console", "催收管理台"),
+    SELECTION("selection", "选查系统"),
+    ;
+
+    private String code;
+    private String description;
+
+    PlatformEnum(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

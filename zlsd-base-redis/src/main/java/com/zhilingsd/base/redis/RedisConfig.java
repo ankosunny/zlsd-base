29b4adbf61aa8e764ac5a863f6feecc3b988da9c
linmenghuai <497706397@qq.com>
2019-10-06 19:25:36 +0800

    修复redisbug

--- zlsd-base-redis/src/main/java/com/zhilingsd/base/redis/RedisConfig.java
+++ zlsd-base-redis/src/main/java/com/zhilingsd/base/redis/RedisConfig.java
@@ -3,7 +3,9 @@ package com.zhilingsd.base.redis;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,10 +13,11 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 @Configuration
+@AutoConfigureAfter(RedisAutoConfiguration.class)
 public class RedisConfig {
 
     @Bean
-    @ConditionalOnProperty("spring.redis.database")
+    //@ConditionalOnProperty("spring.redis.database")
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
 
         // 定义value的序列化方式

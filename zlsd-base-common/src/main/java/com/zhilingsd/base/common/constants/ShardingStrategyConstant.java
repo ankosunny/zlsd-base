9cf3c220fd5cd2214f4461b20563862c80d5e416
yuboliang <12345678>
2019-12-26 14:48:24 +0800

    sharding 相关

---
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/constants/ShardingStrategyConstant.java
@@ -0,0 +1,18 @@
+package com.zhilingsd.base.common.constants;
+
+/**
+ * @author: yuboliang
+ * @date: 2019/12/25
+ **/
+public class ShardingStrategyConstant {
+
+    /**
+     * 雪花ID时间戳右移位数
+     */
+    public static final int SNOW_FLAKE_TIMESTAMP_RIGHT_SHIFT = 24;
+
+    /**
+     * 雪花ID时间纪元
+     */
+    public static final long SNOW_FLAKE_ZLSD_EPOCH = 1510156800000L;
+}

7d7a5530c4be12e97de69dd5df1c0186fb3923fc
yuboliang <12345678>
2020-03-05 17:57:12 +0800

    非空判断

--- zlsd-base-common/src/main/java/com/zhilingsd/base/common/interceptor/FeignTechRequestInterceptor.java
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/interceptor/FeignTechRequestInterceptor.java
@@ -7,6 +7,8 @@ import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Optional;
+
 import static com.zhilingsd.base.common.constants.TechConstant.*;
 
 
@@ -27,10 +29,10 @@ public class FeignTechRequestInterceptor implements RequestInterceptor {
 
         log.debug("用户信息{}", JSONObject.toJSONString(appUserInfo));
 
-        requestTemplate.header(SESSION, appUserInfo.getSession());
-        requestTemplate.header(OPERATOR_ID, String.valueOf(appUserInfo.getOperatorId()));
-        requestTemplate.header(ACCOUNT, String.valueOf(appUserInfo.getAccount()));
-        requestTemplate.header(MERCHANT_ID, String.valueOf(appUserInfo.getMerchantId()));
-        requestTemplate.header(PLATFORM, String.valueOf(appUserInfo.getPlatform()));
+        Optional.ofNullable(appUserInfo.getSession()).ifPresent(tmp -> requestTemplate.header(SESSION, tmp));
+        Optional.ofNullable(appUserInfo.getOperatorId()).ifPresent(tmp -> requestTemplate.header(OPERATOR_ID, String.valueOf(tmp)));
+        Optional.ofNullable(appUserInfo.getAccount()).ifPresent(tmp -> requestTemplate.header(ACCOUNT, tmp));
+        Optional.ofNullable(appUserInfo.getMerchantId()).ifPresent(tmp -> requestTemplate.header(MERCHANT_ID, String.valueOf(tmp)));
+        Optional.ofNullable(appUserInfo.getPlatform()).ifPresent(tmp -> requestTemplate.header(PLATFORM, tmp));
     }
 }

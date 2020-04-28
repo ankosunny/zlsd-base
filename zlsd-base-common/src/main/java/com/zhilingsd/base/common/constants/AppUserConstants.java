29c3ace70de351de84c71f0e0ee7a76413fc5559
yuboliang <12345678>
2020-04-28 15:56:02 +0800

    AppUserConstants

---
+++ zlsd-base-common/src/main/java/com/zhilingsd/base/common/constants/AppUserConstants.java
@@ -0,0 +1,21 @@
+package com.zhilingsd.base.common.constants;
+
+/**
+ * @author: yuboliang
+ * @date: 2020/4/28
+ **/
+public class AppUserConstants {
+
+    /**
+     * 相当于token
+     */
+    public static final  String session = "0";
+    /**
+     * 当前登录用户Id
+     */
+    public static final  Long operatorId = 0L;
+    /**
+     * 当前登录用户账号
+     */
+    public static final  String account = "0";
+}
